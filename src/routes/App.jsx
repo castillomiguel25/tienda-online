cf6778675d8818ed4c5f643812011e5145cdcb1b
castillomiguel25 <miguelangelcastillo947@gmail.com>
2023-06-14 22:30:27 -0400

    'prueba'

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,25 +1,11 @@
 import React, { useState } from 'react';
 import Home from '@pages/HomeUser';
 import House from '../../pages/HomeInitial';
-// import firebaseApp from '../../pages/Callfirebase/firebase';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore';
 
-// const auth = getAuth(firebaseApp);
-// const firestore = getFirestore(firebaseApp);
-
-// console.log(firestore);
 
 function App() {
   const [user, setUser] = useState(null);
 
-  // onAuthStateChanged(auth, (userFirebase) => {
-  //   if (userFirebase) {
-  //     setUser(userFirebase);
-  //   } else {
-  //     setUser(null);
-  //   }
-  // });
 
   return <>{user ? <Home user={user} /> : <House />} </>;
 }
